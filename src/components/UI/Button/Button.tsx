8030e1e977826e149ef8b5bdc56bec13c7ca2f8d
Be Anjara Houssen <hbeanjarahoussen@gmail.com>
2022-06-09 17:04:49 +0300

    feat fix button bg

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -20,8 +20,9 @@ const Button: FC<ButtonProps> = ({
   ...props
 }) => {
   const classes = classNames(
-    "bg-emerald-500  text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150",
+    "text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150",
     { "bg-transparent": transparent },
+    { "bg-emerald-500 ": !transparent },
     { "text-red-600": textColor === "red" },
     {
       "text-white": textColor === "white",
