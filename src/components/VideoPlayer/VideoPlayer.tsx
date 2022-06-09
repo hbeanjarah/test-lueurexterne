8bb0ac4f9a5e11990c5f5dcb8390027836f10b9b
Be Anjara Houssen <hbeanjarahoussen@gmail.com>
2022-06-09 15:59:39 +0300

    feat control video

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,17 +1,17 @@
 import React from "react";
+import { MessagesType } from "types";
 interface VideoProps {
   videoRef: React.MutableRefObject<HTMLVideoElement | null>;
+  handleVideoEnd: () => void;
 }
-const VideoPlayer = ({ videoRef }: VideoProps) => {
+const VideoPlayer = ({ videoRef, handleVideoEnd }: VideoProps) => {
   return (
     <>
       <video
         src="https://setem.fr/wp-content/uploads/2022/05/1065464989-hd_4.mp4"
         ref={videoRef}
         className="brightness-100"
-        onPlaying={(e) => {
-          console.log("dsdsdsds", e.timeStamp);
-        }}
+        onEnded={handleVideoEnd}
       />
     </>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,8 @@ import { MessagesType } from "types";
 
 const Home = () => {
   const [open, setOpen] = useState(true);
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState<boolean>(false);
+  const [showByeMessage, setShowByeMessage] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   const [messages, setMessages] = useState<MessagesType>({
@@ -16,23 +18,51 @@ const Home = () => {
   const handleClickOK = () => {
     videoRef.current?.play();
     setOpen(false);
+    setTimeout(() => {
+      setShowWelcomeMessage(true);
+      videoRef.current?.pause();
+    }, 5000);
   };
 
   const handleContinue = () => {
+    setShowWelcomeMessage(false);
     videoRef.current?.play();
+    setTimeout(() => {
+      setShowByeMessage(true);
+      setShowWelcomeMessage(false);
+    }, 5000);
+  };
+
+  const handleVideoEnd = () => {
+    setMessages({
+      bye: "",
+      welcome: "",
+    });
+    setShowByeMessage(false);
+    setShowWelcomeMessage(false);
+    setOpen(true);
   };
 
   return (
     <div className="text-2xl">
-      <VideoPlayer videoRef={videoRef} />
+      <VideoPlayer handleVideoEnd={handleVideoEnd} videoRef={videoRef} />
 
-      <div className="w-64 h-w-64 flex justify-center flex-col bg-transparent relative mr-auto ml-auto mt-[-150px]">
-        <div className="w-full mb-5 text-center text-3xl font-bold text-white">
-          Message
-        </div>
-        <Button textColor="white" onClick={handleContinue}>
-          Continuer
-        </Button>
+      <div className="w-64 h-w-64 flex justify-center flex-col bg-transparent absolute mr-auto ml-auto mt-[-150px] left-0 right-0">
+        {showWelcomeMessage && (
+          <>
+            <div className="w-full mb-5 text-center text-3xl font-bold text-white">
+              {messages.welcome}
+            </div>
+            <Button textColor="white" onClick={handleContinue}>
+              Continuer
+            </Button>
+          </>
+        )}
+        {showByeMessage && (
+          <div className="w-full mb-5 text-center text-3xl font-bold text-white">
+            {messages.bye}
+          </div>
+        )}
       </div>
 
       <Modal
