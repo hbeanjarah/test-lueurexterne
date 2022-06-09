70aaba538b8fd9b20320db487e2730c7fcad30c4
Be Anjara Houssen <hbeanjarahoussen@gmail.com>
2022-06-09 17:27:36 +0300

    feat fix button close bg

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -20,8 +20,6 @@ const Modal = ({
 
   const enable = () => messages.bye !== "" && messages.welcome !== "";
 
-  console.log("messages", messages);
-
   return open ? (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -64,8 +62,9 @@ const Modal = ({
             <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
               <Button
                 transparent
-                textColor="red"
+                textColor="white"
                 onClick={() => setOpen(false)}
+                customClasses="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
               >
                 Fermer
               </Button>
